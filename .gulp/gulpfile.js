e6c46fc1d1881662432db20f19fe34367e412f6a
Ramsay Lanier <ramsay@nclud.com>
2015-06-06 09:47:22 -0400

    initial router setup

--- .gulp/gulpfile.js
+++ .gulp/gulpfile.js
@@ -6,7 +6,7 @@ gulp.task('sass', function () {
     return gulp.src('../client/assets/scss/**/*.scss')
         .pipe(sass({errLogToConsole: true}))
         .pipe(autoprefixer('last 2 version', 'ie 8', 'ie 9', 'ios 6', 'android 4'))
-        .pipe(gulp.dest('../client/css'))
+        .pipe(gulp.dest('../client/assets/css'))
 });
 
 gulp.task('default', ['sass'], function () {
