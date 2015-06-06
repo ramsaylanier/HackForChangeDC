45d95fcad235cb98dc4ed55b51d501348296b2ff
Ramsay Lanier <ramsay@nclud.com>
2015-06-06 09:42:57 -0400

    gul setup

--- .gulp/gulpfile.js
+++ .gulp/gulpfile.js
@@ -3,12 +3,12 @@ var gulp = require('gulp'),
     sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-    return gulp.src('../client/stylesheets/scss/**/*.scss')
+    return gulp.src('../client/assets/scss/**/*.scss')
         .pipe(sass({errLogToConsole: true}))
         .pipe(autoprefixer('last 2 version', 'ie 8', 'ie 9', 'ios 6', 'android 4'))
-        .pipe(gulp.dest('../client/stylesheets/css'))
+        .pipe(gulp.dest('../client/css'))
 });
 
 gulp.task('default', ['sass'], function () {
-    gulp.watch("../client/stylesheets/scss/**/*.scss", ['sass']);
+    gulp.watch("../client/assets/scss/**/*.scss", ['sass']);
 });
